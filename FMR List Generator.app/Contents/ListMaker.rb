63b863fe993575141c8d0de8d9f9c1f36d8129f5
Tarr Lab <tarrlab@gmail.com>
2012-10-21 14:53:42 -0400

    Updates for New Folder Structure
    Made changes to conform to new folder structure. Script now expects
    that folders dragged on will be the _BV folders instead of the subject
    folder with the _BV folder inside.

--- FMR List Generator.app/Contents/ListMaker.rb
+++ FMR List Generator.app/Contents/ListMaker.rb
@@ -7,8 +7,8 @@ textfile_name = textfile_name + ".txt"
 path_list = path_list.split("%%%")
 target_FMR_list = target_FMR_list.split("%%%")
 
-# path_list = ["/Users/deborahjohnson/Dropbox/Matt and Debs Stuff!/Subjects/S01", "/Users/deborahjohnson/Dropbox/Matt and Debs Stuff!/Subjects/S02"]
-# target_FMR_list = ["/Users/deborahjohnson/Dropbox/Matt and Debs Stuff!/Subjects/S01/_BV-S01/KJ_5116_Post_Loc2_firstvol.fmr", "/Users/deborahjohnson/Dropbox/Matt and Debs Stuff!/Subjects/S02/_BV-S02/S2_5116_Post_Loc2_firstvol.fmr"]
+# path_list = ["/Users/Deb/Desktop/NewFolderStructure/_BV-ME27-S1E4", "/Users/Deb/Desktop/NewFolderStructure/_BV-ME26-S1E4"]
+# target_FMR_list = ["/Users/Deb/Desktop/NewFolderStructure/_BV-ME27-S1E4/ME27-S1E4_MotionEncodeExpt4_Run8-Seq5-Order4.fmr", "/Users/Deb/Desktop/NewFolderStructure/_BV-ME26-S1E4/ME26-S1E4_DynamicStaticLOC_Run2-Order1.fmr"]
 
 # Create empty array for "subjects" hash
 subjects = []
@@ -17,7 +17,7 @@ subjects = []
 path_list.each do |dir|
    subjects << { :base_path => dir }
 end
-
+p subjects
 # Copy path to each subject's target FMR file to the "subjects" hash, with key :target_fmr
 target_FMR_list.each_with_index do |targetFMR_path, i|
   subjects[i][:target_fmr] = targetFMR_path
@@ -25,13 +25,10 @@ end
 
 # Populate "subjects" hash with the path to each subject's _BV folder, and the path to all FMR files in each subject's _BV folder
 subjects.each do |subject|
-  # Get the path to each subject's _BV folder, add it to "subjects" hash with key :bv_path
-  base_dir_entries  = Dir.entries(subject[:base_path])
-  subject[:bv_path] = base_dir_entries.grep(/_BV.+/) { |match| File.join(subject[:base_path], match) }.first
-
-  # Search the contents of each subject's _BV folder for files ending in "*.fmr," 
+  # Search the contents of each _BV folder for files ending in "*.fmr," 
   # Copy the paths of matching files to "subjects" hash with key :fmr_files
-  bv_dir_entries      = Dir.entries(subject[:bv_path])
+  
+  bv_dir_entries      = Dir.entries(subject[:base_path])
   subject[:fmr_files] = bv_dir_entries.grep(/^.+fmr/) { |match| File.join(subject[:base_path], match) }
 end
 
