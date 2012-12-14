200f0079e366ebb773d5869cba3c44165ae7dc87
Tarrlab <tarrlab@gmail.com>
2012-12-14 14:38:11 -0500

    Updated script comments, added README

--- FMR List Generator.app/Contents/ListMaker.rb
+++ FMR List Generator.app/Contents/ListMaker.rb
@@ -1,46 +1,51 @@
-path_list       = ARGV[0]
+# This gets the variables passed from the applescript: 
+  # subject_dirs is the list of paths to each _BV folder)
+  # target_FMR_list is the list of paths to each target FMR file)
+  # textfile_name is what the user wants the text file to be named
+
+subject_dirs    = ARGV[0]
 target_FMR_list = ARGV[1]
 textfile_name   = ARGV[2]
 
+# Overwrite the textfile_name variable with the name plus extension ".txt"
 textfile_name = textfile_name + ".txt"
 
-path_list = path_list.split("%%%")
+# Parse the subject_dirs and target_FMR_list strings that were passed from the AppleScript
+subject_dirs = subject_dirs.split("%%%")
 target_FMR_list = target_FMR_list.split("%%%")
-
-# path_list = ["/Users/Deb/Desktop/NewFolderStructure/_BV-ME27-S1E4", "/Users/Deb/Desktop/NewFolderStructure/_BV-ME26-S1E4"]
-# target_FMR_list = ["/Users/Deb/Desktop/NewFolderStructure/_BV-ME27-S1E4/ME27-S1E4_MotionEncodeExpt4_Run8-Seq5-Order4.fmr", "/Users/Deb/Desktop/NewFolderStructure/_BV-ME26-S1E4/ME26-S1E4_DynamicStaticLOC_Run2-Order1.fmr"]
 
 # Create empty array for "subjects" hash
 subjects = []
 
-# Copy the path of each subject's main directory to the "subjects" hash, with key :base_path
-path_list.each do |dir|
+# Copy each path in subject_dirs to the "subjects" hash, with key :base_path
+subject_dirs.each do |dir|
    subjects << { :base_path => dir }
 end
-p subjects
-# Copy path to each subject's target FMR file to the "subjects" hash, with key :target_fmr
+
+# Copy each path in target_FMR_list to the "subjects" hash, with key :target_fmr
 target_FMR_list.each_with_index do |targetFMR_path, i|
   subjects[i][:target_fmr] = targetFMR_path
 end
 
-# Populate "subjects" hash with the path to each subject's _BV folder, and the path to all FMR files in each subject's _BV folder
+# Populate "subjects" hash with the path to all FMR files in each person's _BV folder
 subjects.each do |subject|
-  # Search the contents of each _BV folder for files ending in "*.fmr," 
-  # Copy the paths of matching files to "subjects" hash with key :fmr_files
-  
+  # Search the contents of the subject's _BV folder for files ending in "*.fmr," 
+  # Copy the paths of matching files to "subjects" hash with key :fmr_files 
   bv_dir_entries      = Dir.entries(subject[:base_path])
   subject[:fmr_files] = bv_dir_entries.grep(/^.+fmr/) { |match| File.join(subject[:base_path], match) }
 end
 
-# Make a single array with all FMR files (from all subjects)
+# Make a single array with all FMR files from all subjects (this is what we'll write to the text file)
 all_fmr_files = subjects.map { |subject| subject[:fmr_files] }.flatten
 
+# Make a new text file with the name the user entered, with write capabilities ("w")
 text_file = File.new(textfile_name, "w")
-
+  # Print the total number of FMR files to be procsessed
   text_file.puts(all_fmr_files.size)
+  # Print all FMR file paths
   text_file.puts(all_fmr_files)
+  # Print "TARGETFMRs"
   text_file.puts("TARGETFMRs")
-  
   # Print the path to each subject's target FMR file as many times as the number of FMR files in that subject's _BV directory
   # NOTE: this is the reason for indexing with hash/key above (in case each subject has a different
   # number of FMR files, for example if you have two subjects from separate studies)
@@ -49,5 +54,6 @@ text_file = File.new(textfile_name, "w")
       text_file.puts(subject[:target_fmr])
     end
   end
+# Close the text file
 text_file.close
   
